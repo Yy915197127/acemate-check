e312256e7099a9a48e1534f39a8297c1434bf5c4
yjq <915197127@qq.com>
2025-07-25 18:37:09 +0800

    1

--- uni_modules/ace-revise-popup/utssdk/app-ios/PopupViewController.swift
+++ uni_modules/ace-revise-popup/utssdk/app-ios/PopupViewController.swift
@@ -1,8 +1,7 @@
 import UIKit
 
+class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate   {
-    
     // 弹出视图的容器
     private let containerView: UIView = {
         let view = UIView()
@@ -11,7 +10,7 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     // 标题
     private let titleLabel: UILabel = {
         let label = UILabel()
@@ -21,7 +20,7 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     //说明
     private let remarkLabel: UILabel = {
         let label = UILabel()
@@ -33,14 +32,14 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     // PickerView
     let pickerView: UIPickerView = {
         let picker = UIPickerView()
         picker.translatesAutoresizingMaskIntoConstraints = false
         return picker
     }()
-    
+
     // 取消按钮
     private let cancelButton: UIButton = {
         let button = UIButton(type: .system)
@@ -54,7 +53,7 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         button.layer.borderWidth = 1
         return button
     }()
-    
+
     // 确定按钮
     private let confirmButton: UIButton = {
         let button = UIButton(type: .system)
@@ -66,7 +65,7 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         button.layer.cornerRadius = 8
         return button
     }()
-    
+
     // 按钮容器
     private let buttonStackView: UIStackView = {
         let stackView = UIStackView()
@@ -76,115 +75,127 @@ class PopupViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-    
-    let data = Array(stride(from: 30, through: 0, by: -1))
+
+    // let data = Array(stride(from: 30, through: 0, by: -1))
+    let data = Array(stride(from: 20, through: -20, by: -1))
     var row = 0
     var reviseInfo: ReviseInfo!
-    var onReviseOffsetBlock: ((_ offset : NSNumber) -> Void)!
-    
+    var onReviseOffsetBlock: ((_ offset: NSNumber) -> Void)!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.dataSource = self
         pickerView.delegate = self
         titleLabel.text = reviseInfo.title
-        row = 30 - Int(truncating: reviseInfo.offset)
+        // row = 30 - Int(truncating: reviseInfo.offset)
+        if let index = data.firstIndex(of: Int(reviseInfo.offset)) {
+            row = index
+        }
         pickerView.selectRow(row, inComponent: 0, animated: false)
         setupView()
     }
-    
+
     private func setupView() {
         // 设置半透明背景
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        
+
         // 添加容器视图
         view.addSubview(containerView)
-        
+
         // 容器视图约束 (宽300 高400)
         NSLayoutConstraint.activate([
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            containerView.widthAnchor.constraint(equalToConstant: 300)
+            containerView.widthAnchor.constraint(equalToConstant: 300),
         ])
-        
+
         // 添加标题
         containerView.addSubview(titleLabel)
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
             titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
+            titleLabel.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor, constant: -20),
         ])
-        
-        
+
         // 添加说明
         containerView.addSubview(remarkLabel)
         NSLayoutConstraint.activate([
             remarkLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
-            remarkLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            remarkLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
+            remarkLabel.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor, constant: 20),
+            remarkLabel.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor, constant: -20),
         ])
-        
+
         // 添加PickerView
         containerView.addSubview(pickerView)
         NSLayoutConstraint.activate([
             pickerView.topAnchor.constraint(equalTo: remarkLabel.bottomAnchor, constant: 8),
             pickerView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            pickerView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            pickerView.heightAnchor.constraint(equalToConstant: 150)
+            pickerView.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor, constant: -20),
+            pickerView.heightAnchor.constraint(equalToConstant: 150),
         ])
-        
+
         // 添加按钮容器
         containerView.addSubview(buttonStackView)
         buttonStackView.addArrangedSubview(cancelButton)
         buttonStackView.addArrangedSubview(confirmButton)
-        
+
         NSLayoutConstraint.activate([
             buttonStackView.topAnchor.constraint(equalTo: pickerView.bottomAnchor, constant: 8),
-            buttonStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            buttonStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            buttonStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
-            buttonStackView.heightAnchor.constraint(equalToConstant: 44)
+            buttonStackView.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor, constant: 20),
+            buttonStackView.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor, constant: -20),
+            buttonStackView.bottomAnchor.constraint(
+                equalTo: containerView.bottomAnchor, constant: -20),
+            buttonStackView.heightAnchor.constraint(equalToConstant: 44),
         ])
-        
+
         // 添加按钮动作
         cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         confirmButton.addTarget(self, action: #selector(confirmButtonTapped), for: .touchUpInside)
     }
-    
+
     @objc private func cancelButtonTapped() {
         dismiss(animated: true, completion: nil)
     }
-    
+
     @objc private func confirmButtonTapped() {
         // 在这里处理确定按钮的逻辑
         onReviseOffsetBlock(data[row] as NSNumber)
         dismiss(animated: true, completion: nil)
     }
-    
+
     // 返回 pickerView 的列数
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     // 自定义行高
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
         return 50
     }
-    
+
     // 返回每列的行数
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return data.count
     }
-    
+
     // MARK: - UIPickerViewDelegate
-    
+
     // 返回每行的标题
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)
+        -> String?
+    {
         return String(format: "%d°", data[row])
     }
-    
+
     // 选中某行后的回调
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         self.row = row
     }
-    
+
 }
